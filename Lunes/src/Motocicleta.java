13bc8de3b63d9ad863857c4b16f975cb2d56ac95
Apita49 <jose.apa4996@gmail.com>
2017-06-12 08:26:27 -0400

    La clase Motocicleta ya implementada, con modelo marca y color

--- Lunes/src/Motocicleta.java
+++ Lunes/src/Motocicleta.java
@@ -1,4 +1,35 @@
-
 public class Motocicleta {
+	private String color;
+	private String modelo;
+	private String marca;
 
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public String toString() {
+		return "Motocicleta de la marca: " + marca + ", de modelo: " + modelo
+				+ " y de color " + color;
+	}
 }
